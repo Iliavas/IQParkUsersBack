473f8cbe2d42d502852d3d09aaa15a0ccd1f4f2f
Vasilenko Ilia <il.vsl0110@gmail.com>
2021-02-26 15:05:08 +0300

    change sqllit

--- evgenApp/settings.py
+++ evgenApp/settings.py
@@ -103,12 +103,8 @@ WSGI_APPLICATION = 'evgenApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "EvgenApp",
-        "USER": "postgres",
-        "PASSWORD" : "Ilvas2006",
-        "HOST" : "127.0.1.1",
-        "PORT" : "5432"
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
